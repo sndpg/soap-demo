d5da246db0c90db59a877134d065d48111c46245
sykq <genericmailacc@gmail.com>
2019-09-25 19:58:22 +0200

    init

---
+++ src/main/java/org/psc/soap/soapdemo/configuration/WebServiceConfiguration.java
@@ -0,0 +1,47 @@
+package org.psc.soap.soapdemo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfiguration extends WsConfigurerAdapter {
+    public static final String TARGET_NAMESPACE = "http://schema.soapdemo.soap.psc.org";
+
+    @Value("${service-path:/service/soap}")
+    private String servicePath;
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, servicePath.concat("/*"));
+    }
+
+    @Bean(name = "worker")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema workerSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("WorkerPort");
+        wsdl11Definition.setLocationUri(servicePath);
+        wsdl11Definition.setTargetNamespace(TARGET_NAMESPACE);
+        wsdl11Definition.setSchema(workerSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema workerSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/worker.xsd"));
+    }
+
+}
---
+++ src/main/java/org/psc/soap/soapdemo/endpoint/WorkerEndpoint.java
@@ -0,0 +1,30 @@
+package org.psc.soap.soapdemo.endpoint;
+
+import lombok.extern.slf4j.Slf4j;
+import org.psc.soap.soapdemo.configuration.WebServiceConfiguration;
+import org.psc.soap.soapdemo.schema.GetStatusResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Endpoint
+public class WorkerEndpoint {
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info("WorkerEndpoint constructed");
+    }
+
+    @ResponsePayload
+    @PayloadRoot(namespace = WebServiceConfiguration.TARGET_NAMESPACE, localPart = "getStatusRequest")
+    public GetStatusResponse getStatus() {
+        log.info("getStatus called");
+        GetStatusResponse getStatusResponse = new GetStatusResponse();
+        getStatusResponse.setStatus("OK");
+        return getStatusResponse;
+    }
+
+}
